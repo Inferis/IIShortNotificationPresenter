116f7eeb7b95bb64151099ad9cba9357c3379c80
Inferis <spam@inferis.org>
2014-10-02 15:31:00 +0200

    Added IIShortNotificationConcurrentQueue.

---
+++ IIShortNotificationPresenter/IIShortNotificationConcurrentQueue.h
@@ -0,0 +1,14 @@
+//
+//  IIShortNotificationConcurrentQueue.h
+//  NotificationDemo
+//
+//  Created by Tom Adriaenssen on 02/10/14.
+//  Copyright (c) 2014 Tom Adriaenssen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "IIShortNotificationQueue.h"
+
+@interface IIShortNotificationConcurrentQueue : NSObject<IIShortNotificationQueue>
+
+@end
---
+++ IIShortNotificationPresenter/IIShortNotificationConcurrentQueue.m
@@ -0,0 +1,74 @@
+//
+//  IIShortNotificationConcurrentQueue.m
+//  Copyright (c) 2014 Tom Adriaenssen. All rights reserved.
+//
+
+#import "IIShortNotificationConcurrentQueue.h"
+#import "IIShortNotificationPresenter+Internal.h"
+
+@implementation IIShortNotificationConcurrentQueue {
+    id<IIShortNotificationQueueHandler> _handler;
+    NSMutableArray* _queue;
+    NSUInteger _presenting;
+}
+
+static NSUInteger _limit = 30;
+
+- (instancetype)initWithHandler:(id<IIShortNotificationQueueHandler>)handler
+{
+    self = [super init];
+    if (self) {
+        _handler = handler;
+        _presenting = 0;
+        _queue = [NSMutableArray array];
+    }
+    return self;
+}
+
+- (void)queuePresentation:(IIShortNotificationType)type message:(NSString *)message title:(NSString *)title accessory:(BOOL)accessory completion:(void (^)(IIShortNotificationDismissal))completion
+{
+    if (IsEmpty(message) && IsEmpty(title))
+        return;
+
+    NSMutableDictionary* msg = [@{@"type": @(type)} mutableCopy];
+    if (message) msg[@"message"] = message;
+    if (title) msg[@"title"] = title;
+    if (accessory) msg[@"accessory"] = @YES;
+    if (completion) msg[@"completion"] = completion;
+
+    [_queue insertObject:msg atIndex:0];
+    [self handleQueue];
+}
+
+- (void)dismissedPresentation
+{
+    @synchronized(_queue) {
+        --_presenting;
+    }
+
+    [self handleQueue];
+}
+
+- (void)handleQueue
+{
+    @synchronized(_queue) {
+        if (_presenting >= _limit)
+            return;
+        
+        NSDictionary* msg = [_queue lastObject];
+        [_queue removeLastObject];
+        if (msg) {
+            _presenting++;
+            [_handler handlePresentation:[msg[@"type"] unsignedIntegerValue]
+                                 message:msg[@"message"]
+                                   title:msg[@"title"]
+                               accessory:[msg[@"accessory"] boolValue]
+                              completion:msg[@"completion"]];
+        }
+        else if (_presenting == 0) {
+            [_handler handlePresentationsFinished];
+        }
+    }
+}
+
+@end
